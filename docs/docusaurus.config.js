47a5cc36d1c2e8eca8571e9ba624f7c9318bf1d1
fuyutarow <fuyutarow@gmail.com>
2021-06-10 08:57:37 +0900

    Fix broken links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -51,10 +51,6 @@ module.exports = {
               label: 'Style Guide',
               to: 'docs/',
             },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
           ],
         },
         // {
