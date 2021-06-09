11aaaf8602b96e0de0e4de1c7772f95a28a9fb52
fuyutarow <fuyutarow@gmail.com>
2021-06-10 08:51:09 +0900

    Add docs

---
+++ docs/sidebars.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+module.exports = {
+  docs: {
+    "pq (CLI)": [
+      "pq/01",
+      "pq/02",
+    ],
+    "partiql-rs (cargo)": [
+      'rs/01',
+      'rs/02',
+    ],
+    "partiql.js (npm)": ['mdx'],
+  },
+};
