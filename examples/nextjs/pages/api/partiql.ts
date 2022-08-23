cf0552abd77b5551d8ebc45dfd3a0f9d589dc597
fuyutarow <fuyutarow@gmail.com>
2022-08-23 18:55:24 +0900

    Use cors

--- examples/nextjs/pages/api/partiql.ts
+++ examples/nextjs/pages/api/partiql.ts
@@ -2,8 +2,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Pool } from 'piqel'
+import Cors from 'cors'
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+// https://github.com/vercel/next.js/blob/canary/examples/api-routes-cors/lib/init-middleware.js
+function initMiddleware(middleware: any) {
+  return (req: NextApiRequest, res: NextApiResponse) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
+}
+
+// Initialize the cors middleware
+// https://github.com/vercel/next.js/blob/canary/examples/api-routes-cors/pages/api/cors.js
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET'],
+    origin: 'https://piqel.pages.dev',
+  })
+)
+
 
 export default async (req: NextApiRequest, res: NextApiResponse<unknown>) => {
+  await cors(req, res)
+
   // @ts-ignore
   const params = new URLSearchParams(req.query);
 
