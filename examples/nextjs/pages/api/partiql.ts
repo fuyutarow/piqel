88f6cd3588e2a7955c66a4dfd1deb920a7c744cc
fuyutarow <fuyutarow@gmail.com>
2022-08-23 17:55:06 +0900

    Add examples/nextjs

---
+++ examples/nextjs/pages/api/partiql.ts
@@ -0,0 +1,18 @@
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Pool } from 'piqel'
+
+export default async (req: NextApiRequest, res: NextApiResponse<unknown>) => {
+  const params = new URLSearchParams(req.query as string);
+
+  const url = params.get('url')
+  const query = params.get('q')
+  const r = await fetch(url)
+  const d = await r.json()
+  const data = Pool.new(JSON.stringify(d)).query(query as string) ?? ""
+
+  res.status(200).json(
+    JSON.parse(data)
+  )
+}
